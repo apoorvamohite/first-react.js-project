1f1d9ed1631ce449461e6aedf0545bf4276462d4
pooja <poojamohite025@gmail.com>
2020-05-31 23:41:09 +0530

    Updates

---
+++ precache-manifest.1f7779b9b594ac9d7b244d95351b601d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760a7af0e0204231c5590b66213307df",
+    "url": "/react-monster-giggle/index.html"
+  },
+  {
+    "revision": "d976634397e80d6070c3",
+    "url": "/react-monster-giggle/static/css/main.f83c20ef.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/react-monster-giggle/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-giggle/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d976634397e80d6070c3",
+    "url": "/react-monster-giggle/static/js/main.c57ff88d.chunk.js"
+  },
+  {
+    "revision": "01e01abb0acb7a77f0de",
+    "url": "/react-monster-giggle/static/js/runtime-main.0cf8e96e.js"
+  }
+]);
